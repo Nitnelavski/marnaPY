df11c150d23c38d56978b30ebbcb8292183d9789
Valentin Mounier <valmoun@users.noreply.github.com>
2025-05-13 14:18:18 +0200

    Add package files

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+
+"""
+This package contains modules for the analysis of marnage in EPANET.
+"""
+
+__version__ = "1.0"
+__author__ = "Valentin Mounier"
+
+# Import key modules or functions here if needed
+# from .module_name import some_function
